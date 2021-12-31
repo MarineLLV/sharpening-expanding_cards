2b6a72133c219522361437f439a1349486d1f523
MarineLLV <marine.lucottelevisage@gmail.com>
2021-12-31 15:13:32 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Expanding Cards
+
+![screen](Assets/screen.png)
+
+Technos :
+- HTML
+- CSS
+- Javascript
+***
+
+
+
+![Happy!](https://media.giphy.com/media/TF9z8Id4LJru8/giphy.gif)
---
+++ script.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll('.panel')
+
+//console.log('panels in a node list: ', panels);
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    });
+}
